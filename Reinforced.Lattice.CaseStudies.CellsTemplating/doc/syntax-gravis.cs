5a1de0b0d8f9a35b548e22b1c95f1d65fe058c01
Pavel Novikov <pavel.b.novikov@gmail.com>
2017-03-19 19:04:29 +0700

    templating

--- Reinforced.Lattice.CaseStudies.CellsTemplating/doc/syntax-gravis.cs
+++ Reinforced.Lattice.CaseStudies.CellsTemplating/doc/syntax-gravis.cs
@@ -1,1 +1,5 @@
 conf.Column(c => c.Registered).Format("Registered `moment({@}).fromNow()`");
+// -> Registered 3 days ago
+
+conf.Column(c => c.Registered).Format("Registered moment({@}).fromNow()");
+// -> Registered moment(25 May 2015).fromNow()
